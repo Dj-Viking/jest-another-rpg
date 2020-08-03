095955aceb0ef8a35c4a437e900b2925d1e2969e
DJ Viking <anders.swedishviking@gmail.com>
2020-08-03 03:53:58 -0400

    10.1.4 leading into the Potion.js file

--- __tests__/random.test.js
+++ __tests__/random.test.js
@@ -1,5 +1,25 @@
-const checkIfEqual = require('../lib/random.js');
+const randomNumber = require('../lib/random.js');
 
-test('checks if 10 is equal to 10', () => {
-  expect(checkIfEqual(10, 10)).toBe(true);
+test('Gets random number between 1 and 10', () => {
+  expect(randomNumber()).toBeGreaterThanOrEqual(1);
+  expect(randomNumber()).toBeLessThanOrEqual(10);
 });
+
+
+
+// const compareValues = require('../lib/random.js');
+
+// test('checks if 10 is equal to 10', () => {
+//   //use this line if exporting the anonymous function
+//   //expect(checkIfEqual(10,10)).toBe(true);
+  
+//   //use this line if exporting the property method
+//   expect(compareValues.checkIfEqual(10, 10)).toBe(true);
+// });
+
+
+
+
+//this will fail at first due to checkIfEqual not being a function
+//so we must export this and also add the random.js file to lib dir
+
